2f3e5bc2835fb22de0b99c2f8f967eb4ec3da106
yunan9 <itzcollor@gmail.com>
2025-11-21 02:39:24 -0500

    chore: refactor code

--- core/src/main/java/io/github/yunan9/arena/profile/ProfileGuiceModule.java
+++ core/src/main/java/io/github/yunan9/arena/profile/ProfileGuiceModule.java
@@ -1,7 +1,6 @@
 package io.github.yunan9.arena.profile;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Module;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import io.github.yunan9.arena.profile.factory.ProfileFactory;
 import io.github.yunan9.arena.profile.impl.ProfileImpl;
@@ -11,14 +10,11 @@ import org.jetbrains.annotations.ApiStatus;
 @ApiStatus.Internal
 public final class ProfileGuiceModule extends AbstractModule {
 
-  private static final Module FACTORY_MODULE =
-      new FactoryModuleBuilder()
-          .implement(Profile.class, ProfileImpl.class)
-          .build(ProfileFactory.class);
-
   @Override
   protected void configure() {
-    this.install(FACTORY_MODULE);
+    this.install(new FactoryModuleBuilder()
+            .implement(Profile.class, ProfileImpl.class)
+            .build(ProfileFactory.class));
 
     this.bind(ProfileService.class).to(ProfileServiceImpl.class);
   }
