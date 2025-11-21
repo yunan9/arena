d3e3b25b593aaab84c2194f44874a4d57754d373
yunan9 <itzcollor@gmail.com>
2025-11-20 20:31:23 -0500

    feat: add annotation for better doc

--- core/src/main/java/io/github/yunan9/arena/profile/ProfileGuiceModule.java
+++ core/src/main/java/io/github/yunan9/arena/profile/ProfileGuiceModule.java
@@ -6,7 +6,9 @@ import com.google.inject.assistedinject.FactoryModuleBuilder;
 import io.github.yunan9.arena.profile.factory.ProfileFactory;
 import io.github.yunan9.arena.profile.impl.ProfileImpl;
 import io.github.yunan9.arena.profile.impl.ProfileServiceImpl;
+import org.jetbrains.annotations.ApiStatus;
 
+@ApiStatus.Internal
 public final class ProfileGuiceModule extends AbstractModule {
 
   private static final Module FACTORY_MODULE =
