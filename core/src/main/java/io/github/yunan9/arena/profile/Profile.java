18f7a8d267a1f3d6f6ff51ed3f3792cc6acf5872
yunan9 <itzcollor@gmail.com>
2025-11-21 17:35:51 -0500

    feat: impl base data for profile entity

--- core/src/main/java/io/github/yunan9/arena/profile/Profile.java
+++ core/src/main/java/io/github/yunan9/arena/profile/Profile.java
@@ -1,6 +1,32 @@
 package io.github.yunan9.arena.profile;
 
+import io.github.yunan9.commons.id.Identifiable;
+import io.github.yunan9.commons.l10n.impl.MutableLocalizable;
+import io.github.yunan9.commons.name.impl.MutableNameable;
+import java.util.Locale;
+import java.util.UUID;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 
 @ApiStatus.NonExtendable
-public interface Profile {}
+public interface Profile
+    extends Identifiable<@NotNull UUID>, MutableNameable<@NotNull String>, MutableLocalizable {
+
+  @Override
+  @NotNull
+  UUID getId();
+
+  @Override
+  @NotNull
+  String getName();
+
+  @Override
+  void setName(final @NotNull String name);
+
+  @Override
+  @NotNull
+  Locale getLocale();
+
+  @Override
+  void setLocale(final @NotNull Locale locale);
+}
--- core/src/main/java/io/github/yunan9/arena/profile/impl/ProfileImpl.java
+++ core/src/main/java/io/github/yunan9/arena/profile/impl/ProfileImpl.java
@@ -1,7 +1,56 @@
 package io.github.yunan9.arena.profile.impl;
 
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
 import io.github.yunan9.arena.profile.Profile;
+import java.util.Locale;
+import java.util.UUID;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 
 @ApiStatus.Internal
-public final class ProfileImpl implements Profile {}
+public final class ProfileImpl implements Profile {
+
+  private final UUID id;
+
+  private String name;
+
+  private Locale locale;
+
+  @AssistedInject
+  public ProfileImpl(
+      final @Assisted @NotNull UUID id,
+      final @Assisted @NotNull String name,
+      final @Assisted @NotNull Locale locale) {
+    this.id = id;
+
+    this.name = name;
+
+    this.locale = locale;
+  }
+
+  @Override
+  public @NotNull UUID getId() {
+    return this.id;
+  }
+
+  @Override
+  public @NotNull String getName() {
+    return this.name;
+  }
+
+  @Override
+  public void setName(final @NotNull String name) {
+    this.name = name;
+  }
+
+  @Override
+  public @NotNull Locale getLocale() {
+    return this.locale;
+  }
+
+  @Override
+  public void setLocale(final @NotNull Locale locale) {
+    this.locale = locale;
+  }
+}
