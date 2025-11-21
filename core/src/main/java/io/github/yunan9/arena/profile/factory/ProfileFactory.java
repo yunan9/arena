94da2334779c5663ebae909bb6271908dfebd4ea
yunan9 <itzcollor@gmail.com>
2025-11-20 20:01:44 -0500

    feat: impl profile factory

--- core/src/main/java/io/github/yunan9/arena/profile/factory/ProfileFactory.java
+++ core/src/main/java/io/github/yunan9/arena/profile/factory/ProfileFactory.java
@@ -1,3 +1,18 @@
 package io.github.yunan9.arena.profile.factory;
 
-public interface ProfileFactory {}
+import io.github.yunan9.arena.profile.Profile;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.UUID;
+
+@ApiStatus.NonExtendable
+@ApiStatus.Internal
+@FunctionalInterface
+public interface ProfileFactory {
+
+  @NotNull
+  Profile createProfile(
+      final @NotNull UUID id, final @NotNull String name, final @NotNull Locale locale);
+}
